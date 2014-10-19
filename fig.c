62f46f787091e6d0436d12a622215a2ff2f59a33
michael-axe <mgambc@mail.missouri.edu>
2014-10-18 21:05:44 -0600

    Michael Axe commit

--- fig.c
+++ fig.c
@@ -2,3 +2,4 @@ fig
 This project is for fig class
 the app is much better for git.
 this is the final commit for this assignemnt.
+Or is this the final commit for this assignemnt?(Michael Axe was here)
